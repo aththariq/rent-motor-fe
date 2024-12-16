1f4cd1b20c5352106d15a6bd359106c36824c5d0
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-12-16 17:12:25 +0700

    feat: implement LandingPage with new components for Hero, Statistics, Testimonials, What We Offer, FAQ, and Footer

---
+++ src/components/WhatWeOffer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WhatWeOffer = () => {
+  return <div></div>;
+};
+
+export default WhatWeOffer;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,21 @@
+import HeroSection from "../components/HeroSection";
+import Statistic from "../components/Statistics";
+import Testimonials from "../components/Testimonials";
+import WhatWeOffer from "../components/WhatWeOffer";
+import FaQ from "../components/FaQ";
+import Footer from "../components/Footer";
+
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection />
+      <Statistic />
+      <WhatWeOffer />
+      <Testimonials />
+      <FaQ />
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
