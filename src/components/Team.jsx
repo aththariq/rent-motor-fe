5f158d2e88ea3172393bcba235d91366662f9567
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-12-19 09:25:59 +0700

    Update team member avatar in Team component

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,7 +4,7 @@ const Team = () => {
   const team = [
     {
       avatar:
-        "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
+        "https://imgur.com/GidbcdB.jpg",
       name: "Jihan Aurelia",
       title: "Frontend Developer",
       linkedin: "https://www.linkedin.com/in/jihanaurelia/",
